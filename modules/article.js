b8957ee7dd4ff5f95c3942b2751139836b80088b
unknown <victorha@gmail.com>
2018-09-01 15:45:50 -0400

    initial scrape object created to pass to front

---
+++ modules/article.js
@@ -0,0 +1,25 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var articleSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    link: {
+        type: String,
+        required: true
+    },
+    summary: {
+        type: String,
+    },
+    comment: {
+        type: Schema.Types.ObjectId,
+        ref: "comment"
+    }
+});
+
+var article = mongoose.model("article", articleSchema);
+
+module.exports = article
---
+++ modules/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    article: require("./article"),
+    comment: require("./comment")
+  };
+  
